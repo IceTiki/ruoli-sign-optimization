babbc87471cb78fd61d50739e4fbcf14caed174f
IceTiki <Tiki_Pufferfish@outlook.com>
2021-09-22 11:25:36 +0800

    优化推送模块

--- actions/sendMessage.py
+++ actions/sendMessage.py
@@ -2,16 +2,18 @@ import requests
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+import re
 
 
 # 通知类
 class SendMessage:
-    def __init__(self, con):
-        self.rl = RlMessage(con['rl_email'],
-                            con['rl_emailApiUrl'])
-        self.qmsg = Qmsg(con['qmsg_key'], con['qmsg_qq'], con['qmsg_isGroup'])
-        self.smtp = Smtp(con['smtp_host'], con['smtp_user'],
-                         con['smtp_key'], con['smtp_sender'], con['smtp_receivers'])
+    def __init__(self, con: dict):
+        self.rl = RlMessage(con.get('rl_email'),
+                            con.get('rl_emailApiUrl'))
+        self.qmsg = Qmsg(con.get('qmsg_key'), con.get(
+            'qmsg_qq'), con.get('qmsg_isGroup'))
+        self.smtp = Smtp(con.get('smtp_host'), con.get('smtp_user'),
+                         con.get('smtp_key'), con.get('smtp_sender'), con.get('smtp_receivers'))
 
     def send(self, msg='default_msg', title='default_title'):
         try:
@@ -65,42 +67,58 @@ class RlMessage:
 
 # Qmsg酱通知类
 class Qmsg:
-    def __init__(self, key, qq, isGroup):
-        # config={'key':'*****','qq':'*****','isgroup':0}
+    '''Qmsg发送类'''
+
+    def __init__(self, key: str, qq: str, isGroup: bool = False):
+        """
+        :param key: qmsg密钥
+        :param qq: 接收消息的qq(多个qq以","分隔)
+        :param isGroup: 接收者是否为群
+        """
         self.key = key
         self.qq = qq
         self.isGroup = isGroup
         self.configIsCorrect = self.isCorrectConfig()
 
     def isCorrectConfig(self):
-        # 简单检查key和qq是否合法
-        for item in [self.key, self.qq]:
-            if not type(item) == str:
-                return 0
-            if len(item) == 0:
-                return 0
-            if "*" in item:
-                return 0
-        return 1
+        """简单检查配置是否合法"""
+        if type(self.key) != str:
+            return 0
+        elif type(self.qq) != str:
+            return 0
+        elif not re.match('^[0-9a-f]{32}$', self.key):
+            return 0
+        elif not re.match('^\d+(,\d+)*$', self.qq):
+            return 0
+        else:
+            return 1
 
     def send(self, msg):
+        """发送消息
+        :param msg: 要发送的消息(自动转为字符串类型)"""
         # msg：要发送的信息|消息推送函数
         msg = str(msg)
         # 简单检查配置
-        if self.configIsCorrect:
+        if not self.configIsCorrect:
+            print('Qmsg配置错误，信息取消发送')
+        else:
             sendtype = 'group/' if self.isGroup else 'send/'
             res = requests.post(url='https://qmsg.zendee.cn/'+sendtype +
                                 self.key, data={'msg': msg, 'qq': self.qq})
             return str(res)
-            #    code = res.json()['code']
-            #    print(code)
-        else:
-            print('Qmsg配置出错')
-            return 'Qmsg配置出错'
 
 
 class Smtp:
-    def __init__(self, host, user, key, sender, receivers):
+    '''Smtp发送类'''
+
+    def __init__(self, host: str, user: str, key: str, sender: str, receivers: list[str]):
+        """
+        :param host: SMTP的域名
+        :param user: 用户名
+        :param key: 用户的密钥
+        :param sender: 邮件发送者(邮箱)
+        :param receivers: 邮件接收者列表(邮箱)
+        """
         self.host = host
         self.user = user
         self.key = key
@@ -121,8 +139,16 @@ class Smtp:
                 return 0
         return 1
 
-    def sendmail(self, msg, title):
-        if self.configIsCorrect:
+    def sendmail(self, msg, title='no title'):
+        """发送邮件
+        :param msg: 要发送的消息(自动转为字符串类型)
+        :param title: 邮件标题(自动转为字符串类型)"""
+        msg = str(msg)
+        title = str(title)
+        if not self.configIsCorrect:
+            print('邮件配置出错')
+            return '邮件配置出错'
+        else:
             mail = MIMEText(msg, 'plain', 'utf-8')
             mail['Subject'] = Header(title, 'utf-8')
 
@@ -131,6 +157,3 @@ class Smtp:
             smtpObj.login(self.user, self.key)
             smtpObj.sendmail(self.sender, self.receivers, mail.as_string())
             print("邮件发送成功")
-        else:
-            print('邮件配置出错')
-            return '邮件配置出错'
